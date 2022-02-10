19986601fde0a353fdde85ebb49ae672c749eab9
Aviciena Santoso <avi.santoso@gmail.com>
2022-02-10 10:49:36 +0800

    revert back to using react-pdf src

--- src/PlanAnnotator/PlanAnnotator.tsx
+++ src/PlanAnnotator/PlanAnnotator.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, Spinner } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
 import { Document } from "react-pdf";
 
@@ -7,13 +7,13 @@ import AnnotatedPage from "./AnnotatedPage";
 import PageData from "../data/PageData";
 import PlanAnnotatorState from "../state/PlanAnnotatorState";
 
-function bto64(blob: Blob) {
-  return new Promise((resolve, _) => {
-    const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result);
-    reader.readAsDataURL(blob);
-  });
-}
+// function bto64(blob: Blob) {
+//   return new Promise((resolve, _) => {
+//     const reader = new FileReader();
+//     reader.onloadend = () => resolve(reader.result);
+//     reader.readAsDataURL(blob);
+//   });
+// }
 
 interface PlanAnnotatorProps {
   src: string;
@@ -21,35 +21,35 @@ interface PlanAnnotatorProps {
 }
 
 function PlanAnnotator({ src, getPages }: PlanAnnotatorProps) {
-  const [b64, setB64] = React.useState<string>();
+  // const [b64, setB64] = React.useState<string>();
   const [numPages, setNumPages] = React.useState<number>();
   const state = React.useMemo(() => new PlanAnnotatorState(getPages()), [
     getPages
   ]);
   const { iconSize, pages } = state;
 
-  React.useEffect(() => {
-    async function init() {
-      const response = await fetch(src);
-      const blob = await response.blob();
-      const b64 = (await bto64(blob)) as string;
-      setB64(b64);
-    }
-    init();
-  }, [src]);
+  // React.useEffect(() => {
+  //   async function init() {
+  //     const response = await fetch(src);
+  //     const blob = await response.blob();
+  //     const b64 = (await bto64(blob)) as string;
+  //     setB64(b64);
+  //   }
+  //   init();
+  // }, [src]);
 
-  if (!b64) {
-    return (
-      <Spinner
-        m={4}
-        thickness="4px"
-        speed="0.5s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-      />
-    );
-  }
+  // if (!b64) {
+  //   return (
+  //     <Spinner
+  //       m={4}
+  //       thickness="4px"
+  //       speed="0.5s"
+  //       emptyColor="gray.200"
+  //       color="blue.500"
+  //       size="xl"
+  //     />
+  //   );
+  // }
 
   function renderPages() {
     return Array.from(new Array(numPages)).map((_, index) => {
@@ -68,7 +68,7 @@ function PlanAnnotator({ src, getPages }: PlanAnnotatorProps) {
 
   return (
     <Box>
-      <Document file={b64} onLoadSuccess={(pdf) => setNumPages(pdf.numPages)}>
+      <Document file={src} onLoadSuccess={(pdf) => setNumPages(pdf.numPages)}>
         {numPages ? renderPages() : "Loading..."}
       </Document>
     </Box>
